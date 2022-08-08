99ad5b39f69112354027b8f085a1cdb73de79cc4
pdenardi <patricio.denardi@hotmail.com>
2022-08-08 18:17:15 -0300

    Fix imports

--- src/main/java/com/padn/shareitems/commands/UserCommands.java
+++ src/main/java/com/padn/shareitems/commands/UserCommands.java
@@ -1,22 +1,22 @@
 package com.padn.shareitems.commands;
 
 import com.padn.shareitems.ShareItem;
+import cloud.commandframework.Command.Builder;
+import cloud.commandframework.Command;
 import cloud.commandframework.meta.CommandMeta;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 
 public class UserCommands {
 
-    public <BukkitCommandManager> UserCommands(BukkitCommandManager manager, Command.Builder<CommandSender> builder) {
+    public <BukkitCommandManager> UserCommands(BukkitCommandManager manager, Builder<CommandSender> builder) {
 
         manager.command(builder.literal("shareitem")
                 .meta(CommandMeta.DESCRIPTION, "Show the item in your hand.")
                 .senderType(Player.class)
                 .permission("shareitems.use")
                 .handler(commandContext -> ShareItem.showOnChat((Player) commandContext.getSender())));
-
 
     }
 
